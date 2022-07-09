a8ee3cc1e03c3838405bdad8a2d75407a9209b5c
Christopher Tavolazzi <ctavolazzi@gmail.com>
2022-07-09 13:02:44 -0700

    Implement LearningOptions link

--- src/actionProvider.js
+++ src/actionProvider.js
@@ -6,28 +6,36 @@ class actionProvider {
    stateRef,
    createCustomMessage,
    ...rest
- ) {
+  ) {
    this.createChatBotMessage = createChatBotMessage;
    this.setState = setStateFunc;
    this.createClientMessage = createClientMessage;
    this.stateRef = stateRef;
    this.createCustomMessage = createCustomMessage;
- }
+  };
 
- greet() {
-  const greetingMessage = this.createChatBotMessage("Hi, friend.")
-  this.updateChatbotState(greetingMessage)
-}
+  greet() {
+    const greetingMessage = this.createChatBotMessage("Hi, friend.")
+    this.updateChatbotState(greetingMessage)
+  };
 
-updateChatbotState(message) {
+  handleJavascriptList = () => {
+    const message = this.createChatBotMessage(
+      "Fantastic, I've got the following resources for you on Javascript:",
+      {
+        widget: "javascriptLinks",
+      }
+    );
 
-// NOTE: This function is set in the constructor, and is passed in      // from the top level Chatbot component. The setState function here     // actually manipulates the top level state of the Chatbot, so it's     // important that we make sure that we preserve the previous state.
+    this.updateChatbotState(message);
+  };
 
-
- this.setState(prevState => ({
-    ...prevState, messages: [...prevState.messages, message]
-  }))
-}
+  updateChatbotState(message) {
+  // NOTE: This function is set in the constructor, and is passed in      // from the top level Chatbot component. The setState function here     // actually manipulates the top level state of the Chatbot, so it's     // important that we make sure that we preserve the previous state.
+    this.setState(prevState => ({
+      ...prevState, messages: [...prevState.messages, message]
+    }))
+  };
 
 }
 
