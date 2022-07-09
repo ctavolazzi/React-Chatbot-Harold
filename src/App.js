66be3d1a9f158b9f8f232eeddb2c770e3e6722f6
Christopher Tavolazzi <ctavolazzi@gmail.com>
2022-07-09 12:50:38 -0700

    V 0.1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import Chatbot from 'react-chatbot-kit'
+import 'react-chatbot-kit/build/main.css';
+
+import actionProvider from './actionProvider';
+import messageParser from './messageParser';
+import config from './config';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot />
+        <Chatbot config={config} actionProvider={actionProvider} messageParser={messageParser} />
       </header>
     </div>
   );
 }
 
 export default App;
-
-//test
